e84828811dea0fa454d632ef629d487cbd074c40
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-06-20 22:18:45 +0800

    图标区域逻辑实现

--- src/Page/home/components/HomeIcons.vue
+++ src/Page/home/components/HomeIcons.vue
@@ -1,68 +1,88 @@
 <template>
     <div class="icons">
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="//s.qunarzz.com/homenode/images/touchheader/hotel.png">
-            </div>
-            <p class="icon-desc">酒店</p>
-        </div>
+        <swiper>
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl">
+                    </div>
+                    <p class="icon-desc">{{ item.desc }}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
 <script>
 export default {
-    name: "HomeIcons"
+    name: "HomeIcons",
+    data() {
+        return {
+            iconList: [
+                {
+                    id: 1001,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/hotel.png",
+                    desc: "酒店"
+                },
+                {
+                    id: 1002,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/flight.png",
+                    desc: "机票"
+                },
+                {
+                    id: 1003,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/train.png",
+                    desc: "火车票"
+                },
+                {
+                    id: 1004,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/package.png",
+                    desc: "度假"
+                },
+                {
+                    id: 1005,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/hotel.png",
+                    desc: "酒店"
+                },
+                {
+                    id: 1006,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/flight.png",
+                    desc: "机票"
+                },
+                {
+                    id: 1007,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/package.png",
+                    desc: "度假"
+                },
+                {
+                    id: 1008,
+                    imgUrl: "//s.qunarzz.com/homenode/images/touchheader/piao.png",
+                    desc: "周边游"
+                }
+            ]
+        }
+    },
+    computed:{
+        pages(){
+            const pages = [];
+            this.iconList.forEach((item, index) => {
+                const page = Math.floor(index / 8);
+                if (!pages[page]) {
+                    pages[page] = []
+                }
+                pages[page].push(item);
+            })
+            return pages
+        }
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '@/assets/style/varibles.styl';
-.icons{
- overflow: hidden;
- height 0px;
- padding-bottom: 50%
+.icons >>> .swiper-container{
+    height: 0
+    padding-bottom: 50%
 }
 .icons .icon{
     position relative
@@ -95,5 +115,6 @@ export default {
     line-height: .44rem;
     color: $darkText;
     text-align: center
+    ellipsis();
 }
 </style>
