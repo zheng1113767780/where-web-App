1005601d1aa1ce11a50e613f8f02ec607129584d
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-06-26 22:46:54 +0800

    城市列表布局

--- src/Page/city/HomeCity.vue
+++ src/Page/city/HomeCity.vue
@@ -2,14 +2,16 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
+import CityList from './components/CityList.vue'
 import CitySearch from './components/CitySearch.vue'
     export default {
-  components: { CityHeader, CitySearch },
+  components: { CityHeader, CitySearch, CityList },
         name:"HomeCity"
     }
 </script>
