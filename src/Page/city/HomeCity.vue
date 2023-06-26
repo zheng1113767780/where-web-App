d9dbec2dac8a6fc7ebb147debbda2c81f13c8b5c
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-06-26 21:49:43 +0800

    城市搜索框

--- src/Page/city/HomeCity.vue
+++ src/Page/city/HomeCity.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader.vue'
+import CitySearch from './components/CitySearch.vue'
     export default {
-  components: { CityHeader },
+  components: { CityHeader, CitySearch },
         name:"HomeCity"
     }
 </script>
