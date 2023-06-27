e03cb8553a10d04f17eb184e233887bf4bf35cdd
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-06-27 14:48:50 +0800

    使用better-scroll给字母表布局

--- src/Page/city/HomeCity.vue
+++ src/Page/city/HomeCity.vue
@@ -3,15 +3,17 @@
         <city-header></city-header>
         <city-search></city-search>
         <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
+import CityAlphabet from './components/CityAlphabet.vue'
 import CityHeader from './components/CityHeader.vue'
 import CityList from './components/CityList.vue'
 import CitySearch from './components/CitySearch.vue'
     export default {
-  components: { CityHeader, CitySearch, CityList },
+  components: { CityHeader, CitySearch, CityList, CityAlphabet },
         name:"HomeCity"
     }
 </script>
---
+++ src/Page/city/components/CityAlphabet.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name:"CityAlphabet"
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import '@/assets/style/varibles.styl';
+.list
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ position: absolute;
+ top 1.58rem;
+ right: 0px;
+ bottom: 0px;
+ width: .4rem;
+ .item
+  text-align: center;
+  line-height: .4rem;
+  color: $bgColor;
+</style>
