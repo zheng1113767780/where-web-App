b0c9a84533abeba1d0e19d901e191790f0db167c
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-06-28 19:36:25 +0800

    兄弟组件传递数据-提高列表性能

--- src/Page/city/components/CityAlphabet.vue
+++ src/Page/city/components/CityAlphabet.vue
@@ -1,55 +1,59 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="item of letters" :key="item" :ref="item"
-        @click="handleLetterClick"
-        @touchstart="handleTouchStart"
-        @touchmove="handleTouchMove"
-        @touchend="handleTouchEnd"
-        >{{ item }}</li>
+        <li class="item" v-for="item of letters" :key="item" :ref="item" @click="handleLetterClick"
+            @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">{{ item }}</li>
     </ul>
 </template>
 
 <script>
-    export default {
-        name:"CityAlphabet",
-        props:['cities'],
-        data() {
-            return {
-                touchStatus:false,
-
+export default {
+    name: "CityAlphabet",
+    props: ['cities'],
+    data() {
+        return {
+            touchStatus: false,
+            startY: 0,
+            timer: null
+        }
+    },
+    updated() {
+        this.startY = this.$refs['A'][0].offsetTop;
+    },
+    computed: {
+        letters() {
+            const letters = []
+            for (let i in this.cities) {
+                letters.push(i)
             }
+            return letters;
+        }
+    },
+    methods: {
+        handleLetterClick(e) {
+            this.$emit("change", e.target.innerText)
         },
-        computed: {
-            letters() {
-                const letters = []
-                for(let i in this.cities){
-                    letters.push(i)
+        handleTouchStart() {
+            this.touchStatus = true;
+        },
+        handleTouchMove(e) {
+            if (this.touchStatus) {
+                if (this.timer) {
+                    clearTimeout(this.timer);
                 }
-                return letters;
-            }
-        },
-        methods:{
-            handleLetterClick(e){
-                this.$emit("change",e.target.innerText)
-            },
-            handleTouchStart(){
-                this.touchStatus = true;
-            },
-            handleTouchMove(e){
-                if(this.touchStatus) {
-                    const startY = this.$refs['A'][0].offsetTop
+                this.timer = setTimeout(() => {
                     const touchY = e.touches[0].clientY - 79
-                    const index = Math.floor((touchY - startY) / 20)
-                    if(index >= 0 && index < this.letters.length){
-                        this.$emit('change',this.letters[index]);
+                    const index = Math.floor((touchY - this.startY) / 20)
+                    if (index >= 0 && index < this.letters.length) {
+                        this.$emit('change', this.letters[index]);
                     }
-                }
-            },
-            handleTouchEnd(){
-                this.touchStatus = false;
+                }, 16)
             }
+        },
+        handleTouchEnd() {
+            this.touchStatus = false;
         }
     }
+}
 </script>
 
 <style lang="stylus" scoped>
