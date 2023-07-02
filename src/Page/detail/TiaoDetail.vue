827ddaa03d3c5689f0e7826166148e9c0a2bb0c6
zheng1113767780 <131132363+zheng1113767780@users.noreply.github.com>
2023-07-02 09:57:59 +0800

    使用递归组件显示详情页列表

--- src/Page/detail/TiaoDetail.vue
+++ src/Page/detail/TiaoDetail.vue
@@ -2,17 +2,49 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/DetailBanner.vue'
 import DetailHeader from './components/DetailHeader.vue'
-    export default {
-  components: { DetailBanner, DetailHeader },
-        name:"TiaoDetail"
+import DetailList from './components/DetailList.vue'
+export default {
+    components: { DetailBanner, DetailHeader, DetailList },
+    name: "TiaoDetail",
+    data() {
+        return {
+            list: [
+                {
+                    title: "成人票",
+                    children:[
+                        {
+                            title:"成人三馆联票",
+                            children:[
+                                {
+                                    title:"成人三馆联票 - 某一连锁店销售"
+                                }
+                            ]
+                        },
+                        {
+                            title:"成人五馆联票"
+                        }
+                    ]
+                },
+                {
+                    title: "学生票"
+                }, {
+                    title: "儿童票"
+                }, {
+                    title: "特惠票"
+                }
+            ]
+        }
     }
+}
 </script>
 
 <style lang="stylus" scoped>
